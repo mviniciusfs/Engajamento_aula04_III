a9ebd4fb9abe8341c207e767d886f789a71d8364
Marcos_Vinici10 <viniciusfigueiredo@ucl.br>
2020-08-26 20:48:07 +0000

    v2

--- main.cs
+++ main.cs
@@ -15,6 +15,21 @@ Deseja-se saber:
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Votação");
+    Console.WriteLine();
+
+    int voto = 0;
+    new
+
+    do
+    {
+      Console.WriteLine("Escolha seu candidato: \n 33 - José Couve \n 25 - Joana Bravo \n 10 - Roberto Nove \n 0 - Voto em Branco \n 4 - Voto Nulo");
+      voto = int.Parse(Console.ReadLine());
+      Console.WriteLine();
+
+    }
+
+
+
   }
 }

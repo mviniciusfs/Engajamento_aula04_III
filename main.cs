7a0ce1c1074b135478827dc8f772c5460a214368
Marcos_Vinici10 <viniciusfigueiredo@ucl.br>
2020-08-26 01:24:52 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+/*
+3- Crie um programa que funcione para uma votação eleitoral. Existem três candidatos:
+33 - José Couve
+25 - Joana Bravo
+10 - Roberto Nove
+0 - Voto branco
+4 - Voto nulo
+
+Deseja-se saber:
+- O candidato vencedor;
+- O total de votos em branco;
+- O total de votos nulos;
+*/
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
